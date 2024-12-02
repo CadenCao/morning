afa04d1a840f7b5f3f1943e13571834e389dcb9c
CadenCao <96766174+CadenCao@users.noreply.github.com>
2024-12-03 00:10:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ app_secret = '1660ddba6b8258c445dc10bf44261cbd'
 
 user_id = 'oZBrP6Ebt1GcssGf2Yf_JiUFbXKg'
 # user_id = os.environ["USER_ID"]
-template_id1 = 'ZtC9zan8wTC0WpAiVub47wryi9yxlEazqNHzPlgpUk8'
-template_id2 = 'kdHK7YLg7cXgq8dI7Uv_sWw2wKqEPsc0tDv9kxV5ENA'
+template_id1 = 'iQiMiZvrc44kqbqR3sxkgWKOCoe4gdG_frxkki0FQAg	'
+template_id2 = 'BHEUpLtIi-ef0qsdsHdmSqI8LnuwuP7x3cGFSvInfE0'
 # template_id = os.environ["TEMPLATE_ID"]
 
 key='183ab5e8e01d6f3876be06ad207c7298'
