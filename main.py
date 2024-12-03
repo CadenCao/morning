9cf5ff6a4158559b93236dfee064dab7e58245c0
CadenCao <96766174+CadenCao@users.noreply.github.com>
2024-12-03 11:10:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ print(love_days,birthday_left,words)
 
 code_city1 = ('440112','黄浦区')
 code_city2 = ('440106','天河区')
-city1,weather1,temperature1,winddirection1,windpower1,humidity1,city_forecasts1,dayweather_forecasts1,nightweather_forecasts1,daytemp_forecasts1,nighttemp_forecasts1 \
+city1,weather1,temperature1,winddirection1,windpower1,humidity1,city_forecasts1,dayweather_forecasts1,nightweather_forecasts1,daytemp_forecasts1,nighttemp_forecasts1, \
 city2,weather2,temperature2,winddirection2,windpower2,humidity2,ity_forecasts2,dayweather_forecasts2,nightweather_forecasts2,daytemp_forecasts2,nighttemp_forecasts2= two_city_weather(code_city1,code_city2)
 
 
